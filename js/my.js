12ac9dd88bd4461c3f958dadd45bace6985e1a87
BernardoBWeiss <142499089+BernardoBWeiss@users.noreply.github.com>
2023-11-27 14:41:24 +0000

    aSAD

---
+++ js/my.js
@@ -0,0 +1,18 @@
+con
+const form = document.getElementById('formulario');
+
+form.
+
+    form.
+    addEventListener('submit', function (event) {
+        event.preventDefault();
+        const formData = new FormData(form);
+        const data = {};
+        formData.
+            formData.
+            forEach((value, key) => {
+                data[key] = value;
+            });
+
+
+        data[ke
